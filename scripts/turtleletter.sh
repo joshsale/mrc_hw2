82629e0a9bc4194a31e0849dabc4e1a823b57ee9
joshsale <sale.joshua@gmail.com>
2018-04-12 23:38:55 -0700

    Adding turtleletter.sh to the repository

---
+++ scripts/turtleletter.sh
@@ -0,0 +1,4 @@
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, -4.5]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, 4.3]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[1.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, -4.3]'
